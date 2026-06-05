66d2b07183f480cfbaec94df8dff0c3ad504dc8b
thatisrikanth <93171358+thatisrikanth@users.noreply.github.com>
2026-06-05 12:18:40 +0530

    Create main.tf

---
+++ terraform/modules/iam/main.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "${var.environment}-ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${var.environment}-ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+    }]
+  })
+}
+
+output "ecs_execution_role_arn" { value = aws_iam_role.ecs_execution_role.arn }
+output "ecs_task_role_arn" { value = aws_iam_role.ecs_task_role.arn }
