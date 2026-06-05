ad48500a9692eb98067913d973dc81bec719187c
thatisrikanth <93171358+thatisrikanth@users.noreply.github.com>
2026-06-05 12:17:24 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  type        = string
+  description = "The target AWS region for all provisioned infrastructure."
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  type        = string
+  description = "Deployment environment name, used to prefix and tag resources."
+  default     = "production"
+}
+
+variable "container_image" {
+  type        = string
+  description = "The URI of the Docker container image in ECR. Left empty during bootstrapping."
+  default     = ""
+}
